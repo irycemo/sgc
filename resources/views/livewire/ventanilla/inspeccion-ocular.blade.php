38c5f6fc7f066703eee15f2ae7d5b33d7459a073
kikorobledo <enrique_j_@hotmail.com>
2023-08-24 11:58:03 -0600

    Fix ventanilla avaluos

--- resources/views/livewire/ventanilla/inspeccion-ocular.blade.php
+++ resources/views/livewire/ventanilla/inspeccion-ocular.blade.php
@@ -170,7 +170,6 @@
                                 <option value="46">Variación Catastral</option>
                                 <option value="43">Avaluos de desglose de fraccionamientos, condominios, conjuntos habitacionales y subdivisiones</option>
                                 <option value="44">Avaluos de desglose de cualquier otro tipo de inmueble</option>
-                                <option value="47">Avaluos de desglose de cualquier otro tipo de inmueble</option>
                                 <option value="">Avaluos de actualización</option>
                                 <option value="">Avaluos de fusión</option>
                                 <option value="">Avaluos de cambio de régimen</option>
