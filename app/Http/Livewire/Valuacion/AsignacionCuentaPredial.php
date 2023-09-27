82fdb108ebe1f4b500cbd29b7dd8834b68c07fda
kikorobledo <enrique_j_@hotmail.com>
2023-09-27 16:16:56 -0600

    Asignacion de cuenta

--- app/Http/Livewire/Valuacion/AsignacionCuentaPredial.php
+++ app/Http/Livewire/Valuacion/AsignacionCuentaPredial.php
@@ -202,7 +202,7 @@ class AsignacionCuentaPredial extends Component
         }else{
 
             $this->valuadores = User::where('status', 'activo')
-                                    ->where('oficina', auth()->user()->oficina->oficina)
+                                    ->where('oficina', auth()->user()->oficina->id)
                                     ->where('valuador', 1)
                                     ->orderBy('ap_paterno')
                                     ->get();
