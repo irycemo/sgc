e1534dde82b1170297c8049cb88869076a7cf875
kikorobledo <enrique_j_@hotmail.com>
2024-05-28 12:52:44 -0600

    Fix operar

--- app/Livewire/GestionCatastral/RevisionTraslados/RevisarTraslado.php
+++ app/Livewire/GestionCatastral/RevisionTraslados/RevisarTraslado.php
@@ -467,13 +467,13 @@ class RevisarTraslado extends Component
 
         }else{
 
-            if(($pn_adquirientes + $pn + $pp + $pp_transmitentes) != $pp_transmitentes){
+            if(($pn_adquirientes + $pn + $pp + $pp_adquirientes) != $pp_transmitentes){
 
                 throw new Exception("La suma de los porcentajes de nuda debe ser " . $pp_transmitentes . '%.');
 
             }
 
-            if(($pu_adquirientes + $pu + $pp + $pp_transmitentes) != $pp_transmitentes){
+            if(($pu_adquirientes + $pu + $pp + $pp_adquirientes) != $pp_transmitentes){
 
                 throw new Exception("La suma de los porcentajes de usufructo debe ser " . $pp_transmitentes . '%.');
 
