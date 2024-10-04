6f97b5d7ae6a280cd3a744c552c30d24fa58b903
kikorobledo <enrique_j_@hotmail.com>
2024-10-04 14:57:09 -0600

    Fix asignar  cuenta predio ignaorado

--- app/Livewire/Valuacion/AvaluoPredioIgnorado.php
+++ app/Livewire/Valuacion/AvaluoPredioIgnorado.php
@@ -519,9 +519,8 @@ class AvaluoPredioIgnorado extends Component
 
         $cuenta = AsignarCuenta::where('localidad', $this->localidad)
                                 ->where('oficina', $this->oficina)
-                                ->where('tipo', $this->tipo)
+                                ->where('tipo_predio', $this->tipo)
                                 ->where('registro', $this->numero_registro)
-                                ->where('tipo', $this->tipo)
                                 ->where('valuador', auth()->id())
                                 ->first();
 
