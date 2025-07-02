cf3f7f534108cacf71919cf383ec3015dc97186e
irycemo <sgc.irycem@gmail.com>
2025-07-02 12:20:18 -0600

    Fix migracion

--- database/migrations/2025_04_10_140802_create_terrenos_comuns_table.php
+++ database/migrations/2025_04_10_140802_create_terrenos_comuns_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->decimal('area_terreno_comun', 15,4);
             $table->decimal('indiviso_terreno', 15,4);
             $table->decimal('valor_unitario', 15,4);
-            $table->decimal('superficie_proporcional', 15,4);
+            $table->decimal('superficie_proporcional', 15,4)->nullable();
             $table->decimal('valor_terreno_comun', 15,4);
             $table->foreignId('creado_por')->nullable()->references('id')->on('users');
             $table->foreignId('actualizado_por')->nullable()->references('id')->on('users');
