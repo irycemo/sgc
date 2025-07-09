6661507fdfbe70da6c804b7f0073899608479cc0
root <root@DESKTOP-IVHFQKO.localdomain>
2025-07-09 13:31:22 -0600

    Fix impresion consultas

--- app/Livewire/Comun/Consultas/CertificacionesConsulta.php
+++ app/Livewire/Comun/Consultas/CertificacionesConsulta.php
@@ -2,14 +2,63 @@
 
 namespace App\Livewire\Comun\Consultas;
 
-use App\Models\Certificacion;
 use Livewire\Component;
+use App\Models\Certificacion;
+use Illuminate\Support\Facades\Log;
+use App\Enums\Certificaciones\CertificacionesEnum;
+use App\Http\Controllers\Certificaciones\CertificacionesController;
+use App\Http\Controllers\Certificaciones\CertificadoHistoriaController;
+use App\Http\Controllers\Certificaciones\CertificadoNegativoController;
+use App\Http\Controllers\Certificaciones\CertificadoRegistroController;
+use App\Http\Controllers\Certificaciones\CedulaActualizcacionController;
 
 class CertificacionesConsulta extends Component
 {
 
     public $predio_id;
     public $certificaciones;
+
+    public function imprimirCertificacion(Certificacion $modelo){
+
+        try {
+
+            if($modelo->tipo == CertificacionesEnum::NOTIFICACION_VALOR_CATASTRAL){
+
+                $pdf = (new CertificacionesController())->reimprimirNotifiacionValorCatastral($modelo);
+
+            }elseif($modelo->tipo == CertificacionesEnum::CERTIFICADO_CATASTRAL){
+
+            }elseif($modelo->tipo == CertificacionesEnum::CERTIFICADO_HISTORIA){
+
+                $pdf = (new CertificadoHistoriaController())->reimprimirCertificado($modelo);
+
+            }elseif($modelo->tipo == CertificacionesEnum::CEDULA_CATASTRAL){
+
+                $pdf = (new CedulaActualizcacionController())->reimprimirCedula($modelo);
+
+            }elseif($modelo->tipo == CertificacionesEnum::CERTIFICADO_NEGATIVO){
+
+                $pdf = (new CertificadoNegativoController())->reimprimirCertificado($modelo);
+
+            }elseif($modelo->tipo == CertificacionesEnum::CERTIFICADO_REGISTRO){
+
+                $pdf = (new CertificadoRegistroController())->reimprimirCertificado($modelo);
+
+            }
+
+            return response()->streamDownload(
+                fn () => print($pdf->output()),
+                $modelo->tipo->label() . '-' . $modelo->año . '-' .$modelo->folio . '.pdf'
+            );
+
+        } catch (\Throwable $th) {
+
+            Log::error("Error al reimiprimir certificación en consulta por el usuario: (id: " . auth()->user()->id . ") " . auth()->user()->name . ". " . $th);
+            $this->dispatch('mostrarMensaje', ['error', "Ha ocurrido un error."]);
+
+        }
+
+    }
 
     public function mount(){
 
--- resources/views/livewire/comun/consultas/traslados-consulta.blade.php
+++ resources/views/livewire/comun/consultas/traslados-consulta.blade.php
@@ -2,7 +2,7 @@
 
     <h4 class="text-2xl tracking-widest py-1 px-6 text-gray-600 rounded-xl border-b-2 border-gray-500 font-thin mb-6  bg-white">Traslados</h4>
 
-    <div class="bg-white p-4 rounded-lg w-full shadow-lg mb-5 text-sm text-gray-500">
+    <div class="bg-white p-4 rounded-lg w-full shadow-lg mb-5 text-sm text-gray-500" wire:loading.class.delaylongest="opacity-50">
 
         <table class="w-full overflow-x-auto table-fixed">
 
