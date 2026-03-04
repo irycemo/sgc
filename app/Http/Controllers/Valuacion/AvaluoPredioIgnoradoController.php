c90578c9690fca90a092c74c6dfc46ead7c3b195
kikorobledo <enrique_j_@hotmail.com>
2026-03-04 14:59:57 -0600

    Fix mis avaluos

--- app/Http/Controllers/Valuacion/AvaluoPredioIgnoradoController.php
+++ app/Http/Controllers/Valuacion/AvaluoPredioIgnoradoController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Valuacion;
 
 use App\Models\Avaluo;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class AvaluoPredioIgnoradoController extends Controller
@@ -11,6 +10,8 @@ class AvaluoPredioIgnoradoController extends Controller
 
     public function __invoke(Avaluo $avaluo)
     {
+
+        if($avaluo->asignado_a !== auth()->id()) abort(403, 'No tienes asginado este avalúo.');
 
         $id =  $avaluo->getKey() ? $avaluo->id : null;
 
