da3aaa8d5b9cb63fe0b2d38f6612c3cec6d0b71c
kikorobledo <enrique_j_@hotmail.com>
2026-03-04 15:14:41 -0600

    Fix mis avaluos

--- app/Http/Controllers/Valuacion/Valuacion.php
+++ app/Http/Controllers/Valuacion/Valuacion.php
@@ -11,7 +11,11 @@ class Valuacion extends Controller
     public function __invoke(Avaluo $avaluo)
     {
 
-        if($avaluo?->asignado_a !== auth()->id()) abort(403, 'No tienes asginado este avalúo.');
+        if($avaluo){
+
+            if($avaluo->asignado_a !== auth()->id()) abort(403, 'No tienes asginado este avalúo.');
+
+        }
 
         $id =  $avaluo->getKey() ? $avaluo->id : null;
 
