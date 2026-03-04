4de317e8bab5d1a5c053ba1d7450c83a6aca57e1
root <root@DESKTOP-IVHFQKO.localdomain>
2026-03-04 13:17:32 -0600

    Fix avaluos

--- resources/views/livewire/admin/avaluos.blade.php
+++ resources/views/livewire/admin/avaluos.blade.php
@@ -451,6 +451,79 @@
 
     </x-confirmation-modal>
 
+    <x-dialog-modal wire:model="modalVerArchivos" maxWidth="sm">
+
+        <x-slot name="title">
+
+            Archivos
+
+        </x-slot>
+
+        <x-slot name="content">
+
+            <table class="table-auto lg:table-fixed w-full">
+
+                <thead class="border-b border-gray-300 ">
+
+                    <tr class="text-sm text-gray-500 text-left traling-wider whitespace-nowrap">
+
+                        <th class="px-2">Descripción</th>
+                        <th class="px-2">Link</th>
+
+                    </tr>
+
+                </thead>
+
+                <tbody class="divide-y divide-gray-200">
+
+                    @if($this->modelo_administrativo)
+
+                        @foreach ($this->modelo_administrativo->archivos as $archivo)
+
+                            <tr class="text-gray-500 text-sm leading-relaxed">
+                                <td class=" px-2 w-full ">{{ $archivo->descripcion }}</td>
+                                <td class=" p-2 w-full ">
+
+                                    @if(get_class($this->modelo_administrativo) === 'App\Models\VariacionCatastral')
+
+                                        <x-link-blue class="w-min whitespace-nowrap" href="{{ $archivo->getLinkVariacionCatastral() }}" target="_blank">Ver archivo</x-link-blue>
+
+                                    @else
+
+                                        <x-link-blue class="w-min whitespace-nowrap" href="{{ $archivo->getLinkPredioIgnorado() }}" target="_blank">Ver archivo</x-link-blue>
+
+                                    @endif
+                                </td>
+                            </tr>
+
+                        @endforeach
+
+                    @endif
+
+                </tbody>
+
+            </table>
+
+        </x-slot>
+
+        <x-slot name="footer">
+
+            <div class="flex gap-3">
+
+                <x-button-red
+                    wire:click="$toggle('modalVerArchivos')"
+                    wire:loading.attr="disabled"
+                    wire:target="$toggle('modalVerArchivos')"
+                    type="button">
+                    Cerrar
+                </x-button-red>
+
+            </div>
+
+        </x-slot>
+
+    </x-dialog-modal>
+
     @script
 
         <script>
