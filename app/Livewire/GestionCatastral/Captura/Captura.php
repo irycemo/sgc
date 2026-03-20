b3b30333cb514dc2a159643a3b6cb90c0d0d378b
kikorobledo <enrique_j_@hotmail.com>
2026-03-20 13:03:33 -0600

    Fix captura

--- app/Livewire/GestionCatastral/Captura/Captura.php
+++ app/Livewire/GestionCatastral/Captura/Captura.php
@@ -370,7 +370,7 @@ class Captura extends Component
 
     public function validarTitulo(){
 
-        $predio = Predio::whereIn('documento_entrada', $this->predio->documento_entrada)
+        $predio = Predio::where('documento_entrada', $this->predio->documento_entrada)
                             ->where('documento_numero', $this->predio->documento_numero)
                             ->first();
 
