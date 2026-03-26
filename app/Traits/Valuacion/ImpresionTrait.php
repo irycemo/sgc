ac8a6914727c516ccd0b89d816f339af47f4d09c
kikorobledo <enrique_j_@hotmail.com>
2026-03-26 09:21:00 -0600

    Fix impresion notarial

--- app/Traits/Valuacion/ImpresionTrait.php
+++ app/Traits/Valuacion/ImpresionTrait.php
@@ -217,9 +217,9 @@ trait ImpresionTrait
 
         }
 
-        if($this->predio_padre->superficie_total_terreno <= $superficie_terreno){
+        if($this->predio_padre->superficie_notarial <= $superficie_terreno){
 
-            throw new GeneralException('La suma de la superfice de terreno de los avalúos es mayor a la superficie del predio padre.');
+            throw new GeneralException('La suma de la superfice de terreno de los avalúos es mayor a la superficie notarial del predio padre.');
 
         }
 
