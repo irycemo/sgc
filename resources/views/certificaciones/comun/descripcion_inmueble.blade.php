1ef5301b999f63a150ac1ad428955ca0a667d19a
kikorobledo <enrique_j_@hotmail.com>
2026-04-24 15:53:26 -0600

    Fix certificados

--- resources/views/certificaciones/comun/descripcion_inmueble.blade.php
+++ resources/views/certificaciones/comun/descripcion_inmueble.blade.php
@@ -2,16 +2,16 @@
 
 <p class="parrafo">
 
-    <strong>Superficie total de terreno:</strong>  {{ $predio->superficie_total_terreno }} @if(isset($predio->tipo_predio) && $predio->tipo_predio == 2) Hectareas @else Metros cuadrados @endif
+    <strong>Superficie total de terreno:</strong>  {{ $predio->superficie_total_terreno }} @if(isset($predio->tipo_predio) && $predio->tipo_predio == 2) Hectareas @else Metros cuadrados @endif;
 
     <strong>Superficie total de construcción:</strong> {{ $predio->superficie_total_construccion }} Metros cuadrados
 </p>
 
 <p class="parrafo">
 
-    <strong>superficie judicial:</strong>  {{ $predio->superficie_judicial }} @if(isset($predio->tipo_predio) && $predio->tipo_predio == 2) Hectareas @else Metros cuadrados @endif;
+    <strong>superficie notarial:</strong> {{ $predio->superficie_notarial }} @if(isset($predio->tipo_predio) && $predio->tipo_predio == 2) Hectareas @else Metros cuadrados @endif
 
-    <strong>superficie notarial:</strong> {{ $predio->superficie_notarial }} @if(isset($predio->tipo_predio) && $predio->tipo_predio == 2) Hectareas @else Metros cuadrados @endif;
+    <strong>superficie judicial:</strong>  {{ $predio->superficie_judicial }} @if(isset($predio->tipo_predio) && $predio->tipo_predio == 2) Hectareas @else Metros cuadrados @endif;
 
 </p>
 
